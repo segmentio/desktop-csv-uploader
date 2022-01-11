426ab9a2a64169ba331cfb77753f0fb0a8b139fe
Peter Walker <petersinclairwalker@gmail.com>
2022-01-11 16:50:21 -0500

    main fields are now ignored in importer

--- public/importer.js
+++ public/importer.js
@@ -12,21 +12,20 @@ ipcRenderer.on('import-to-segment', (event, data) =>{
 console.log('importing-to-segment')
   let analytics = new Analytics(data.writeKey)
   const {eventField, anonymousIdField, userIdField, timestampField, ...rest } = data
-  // propFields = distillPropFields({eventField, anonymousIdField, userIdField, timestampField}, data.csvData[0])
+  propFields = distillPropFields([eventField, anonymousIdField, userIdField, timestampField], Object.keys(data.csvData[0]))
 
-
-
+  let properties
   for (let i=0; i<data.csvData.length; i++) {
-    console.log(data.csvData[i])
+    properties = {}
+    propFields.map((field) => {properties[field] = data.csvData[i][field]})
+
     if (data.eventTypes['track']) {
       analytics.track({
         event:data.csvData[i][eventField],
         anonymousId:data.csvData[i][anonymousIdField],
         userId:data.csvData[i][userIdField],
         timestamp: new Date(data.csvData[i][timestampField]),
-        // properites: {
-        //   propertyfields: propFields.map()...
-        // }
+        properites: properties
       })
     }
 
@@ -50,17 +49,17 @@ console.log('importing-to-segment')
 // save settings
 // retrieve settings
 
-// function distillPropFields(fieldsToIgnore, anyEvent){
-//   //super slow since each field gets deleted
-//   // TODO explore how we can ignore the irrelevant fields better
-//
-//   const fieldsArray = Object.keys(anyEvent)
-//   let propertiesFields = []
-//     for (const field of fieldsArray){
-//       if (fieldsToIgnore[field]) {
-//       } else {
-//         fieldsArray.append
-//       }
-//     }
-//   return
-// }
+function distillPropFields(fieldsToIgnoreArray, fieldsArray){
+  //super slow since each field gets deleted, but its only done once before then beginning of the import loop
+  // TODO explore how we can ignore the irrelevant fields better
+  let propFields = []
+    for (const field of fieldsArray){
+      if (!fieldsToIgnoreArray.includes(field)) {
+        propFields.push(field)
+      }
+    }
+
+  console.log(fieldsToIgnoreArray)
+  console.log(fieldsArray)
+  return propFields
+}
